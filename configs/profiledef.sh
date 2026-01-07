eeed99d70610d527ae5cca188ef66d0103fdee7a
onenowy <92918392+onenowy@users.noreply.github.com>
2026-01-08 01:39:40 +0900

    Update profiledef.sh

--- configs/profiledef.sh
+++ configs/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archlinux-plasma"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="Arch Linux Plasma"
+iso_application="Arch Linux Plasma Live"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
